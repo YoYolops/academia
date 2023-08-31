9480c654802e53356ea0bb2b20b3401d3090923b
yo <yohanhxh@gmail.com>
2023-08-31 14:57:43 -0300

    Enviando algoritmo ExtendedCountingSort

--- eda-leda/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ eda-leda/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -13,6 +13,7 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
         if(leftIndex >= rightIndex) return;
+        int[] responseArray = new int[(rightIndex - leftIndex) + 1];
 
         int lowestValue = Integer.MAX_VALUE;
         int highestValue = Integer.MIN_VALUE;
@@ -21,10 +22,22 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
             if(array[i] < lowestValue) lowestValue = array[i];
         }
 
+        // Unica alteração necessária com relacao ao counting sort padrao é que o acesso ao frequency array através de indices
+        // armazenados em array[i] deve ser feito pela operação: frequencyArray[ 0-(lowestValue - array[i]) ] pois já
+        // que array[i] possui elementos negativos, acessar frequencyArray[array[i]] poderia resultar em algo como frequencyArray[-2]
         int[] frequencyArray = new int[(highestValue-lowestValue)+1];
         for(int i = leftIndex; i <= rightIndex; i++) frequencyArray[0-(lowestValue - array[i])] += 1;
         for(int i = 1; i < frequencyArray.length; i++) frequencyArray[i] += frequencyArray[i-1];
-        
+        for(int i = rightIndex; i >= leftIndex; i--) {
+            responseArray[frequencyArray[0-(lowestValue - array[i])]-1] = array[i];
+            frequencyArray[0-(lowestValue - array[i])] -= 1;
+        }
+
+        int responseArrayCounter = 0;
+        for(int i = leftIndex; i <= rightIndex; i++) {
+            array[i] = responseArray[responseArrayCounter];
+            responseArrayCounter++;
+        }
 	}
 
 }
--- eda-leda/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/Mani.java
+++ eda-leda/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/Mani.java
@@ -1,10 +1,13 @@
 package sorting.linearSorting;
 
+import java.util.Arrays;
+
 public class Mani {
     public static void main(String[] args) {
         ExtendedCountingSort sorter = new ExtendedCountingSort();
         Integer[] exampleArray = new Integer[]{0,-1,6,4,0,12,44, 44,        12,9,1,0,7,2,5,1,6,9,7,1,2};
 
-        sorter.sort(exampleArray, 0, 7);
+        sorter.sort(exampleArray, 0, 20);
+        System.out.println(Arrays.toString(exampleArray));
     }
 }
